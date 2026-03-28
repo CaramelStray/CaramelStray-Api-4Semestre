0959052d2ffce189d4ddaad326d09cefc85e4415
GiovanaZucareli <giovanazucareli@gmail.com>
2026-03-27 21:34:52 -0300

    [feat] criação do layout da tela de visualização de clientes com dados mockados

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,7 @@
 <script setup lang="ts">
-import { SidebarProvider, SidebarInset } from '@/components/ui/sidebar'
-import AppSidebar from '@/components/layout/Sidebar.vue'
+import Layout from '@/components/layout/Sidebar.vue'
 </script>
 
 <template>
-  <SidebarProvider>
-    <AppSidebar />
-    <SidebarInset>
-      <RouterView />
-    </SidebarInset>
-  </SidebarProvider>
+  <Layout />
 </template>

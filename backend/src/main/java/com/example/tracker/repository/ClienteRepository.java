14f264b8b643efa00bfd4204d013de3f47009ddc
Daniel Lima <danielbkplima@gmail.com>
2026-03-30 19:20:06 -0300

    [feat] adição de filtros de pais e distancia para clientes

--- backend/src/main/java/com/example/tracker/repository/ClienteRepository.java
+++ backend/src/main/java/com/example/tracker/repository/ClienteRepository.java
@@ -2,10 +2,11 @@ package com.example.tracker.repository;
 
 import com.example.tracker.entity.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.Optional;
 
-public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>, JpaSpecificationExecutor<Cliente> {
     Optional<Cliente> findByEmailContato(String emailContato);
     Optional<Cliente> findByDocumento(String documento);
 }
